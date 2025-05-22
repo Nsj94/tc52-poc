e5c659b572006318990c408c60110a45d7f81d06
Narinder Aithinkers <narinderaithinkers@Narinders-MacBook-Pro.local>
2025-05-22 20:45:25 +0530

    feat: enable Chrome inspection in MainActivity and update dependencies

--- android/app/src/main/java/com/demo/zebrascanner/MainActivity.java
+++ android/app/src/main/java/com/demo/zebrascanner/MainActivity.java
@@ -1,5 +1,13 @@
 package com.demo.zebrascanner;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView.setWebContentsDebuggingEnabled(true); // ✅ Enables Chrome inspection
+    }
+}
